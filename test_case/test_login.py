f74ef44b9ae172fc186b0e52c22bed7cfc994a15
wuwenshuai <1592485002@qq.com>
2023-08-23 17:29:22 +0800

    Changes

---
+++ test_case/test_login.py
@@ -0,0 +1,38 @@
+import os
+import time
+import unittest
+
+from selenium import webdriver
+
+from common.comapi import Common
+from public.time_login import time_login
+
+
+class testShouye(unittest.TestCase):
+    @classmethod
+    def testsetUpClass(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("http://aiweinewtest.zizaicloud.cn//login")
+        self.driver.implicitly_wait(100)
+        # self.driver.set_window_size(1724, 1055)
+        self.driver.find_element("xpath", "/html/body/div[1]/div/div/div[2]/div[2]/div/form/div[1]/div/div/input").click()
+        #奇怪，登录页的用户名框，css定位不到，xpath就能；seleiuimIDE用xpath就能定位到
+        # self.driver.find_element("Css",
+        #                          '/html/body/div[1]/div/div/div[2]/div[2]/div/form/div[1]/div/div/input').click()
+
+        self.driver.find_element("xpath", '/html/body/div[1]/div/div/div[2]/div[2]/div/form/div[1]/div/div/input').send_keys("admin87654321")
+        self.driver.implicitly_wait(10)
+        self.driver.find_element("xpath", '/html/body/div[1]/div/div/div[2]/div[2]/div/form/div[2]/div/div/input').click()
+        self.driver.find_element("xpath", '/html/body/div[1]/div/div/div[2]/div[2]/div/form/div[2]/div/div/input').send_keys("WOrkeasy2019.")
+        self.driver.find_element("xpath", "//button").click()
+        self.driver.implicitly_wait(100)
+        time.sleep(5)
+    # @classmethod
+
+
+# 打开首页
+def test_home01(self):
+    time_login(self.driver).login()
+
+if __name__ == "__main__":
+    unittest.main()
