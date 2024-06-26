cc69ce1614e6a66d5a4c69ef8042414c66ea81fa
wuwenshuai <1592485002@qq.com>
2024-06-26 16:38:17 +0800

    增加demo测试

---
+++ test_case/test_10_demo.py
@@ -0,0 +1,39 @@
+import os
+import time
+import unittest
+import random
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common import keys
+from common.comapi import Common
+from public.aiwei_login import aiwei_login
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from public.Retryable import retry_on_failure
+
+
+
+class testTask(unittest.TestCase):
+    @classmethod
+    # @retry_on_failure(max_retries=3, delay=1)
+    def testsetUpClass(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("https://www.baidu.com/")
+        # a = self.driver.find_element(By.CLASS_NAME, 'bg s_btn')
+        # print(a)
+        page_title = self.driver.title
+        print(page_title)
+        time.sleep(100)
+
+
+
+# 打开首页
+def test_home01(self):
+    aiwei_login(self.driver).login()
+
+
+if __name__ == "__main__":
+    unittest.main()
